09651ff860e9885db18a150c1427698f01df222b
li-guohao <git@liguohao.cn>
2023-09-19 20:28:07 +0800

    rename blog zh label to publish

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
           {
             type: 'docsVersionDropdown',
             position: 'right',
-            dropdownItemsAfter: [{to: '/versions', label: 'All versions'}],
+            dropdownItemsAfter: [{ to: '/versions', label: 'All versions' }],
             dropdownActiveClassDisabled: true,
           },
           {

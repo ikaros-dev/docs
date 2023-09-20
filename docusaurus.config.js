01050afc018c8a9b7a04d60ef49bdaae924ae563
li-guohao <git@liguohao.cn>
2023-09-20 10:15:01 +0800

    fix: blog edit url incorrect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ikaros-dev/docs/edit/master/blog/',
+            'https://github.com/ikaros-dev/docs/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
